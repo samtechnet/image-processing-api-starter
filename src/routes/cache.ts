b4c4f2963b6a696ba580a3fa760b4f083d0ce2d7
SAMTECHNET <samlaja1292@gmail.com>
2022-03-22 12:09:38 +0100

    linting editing

--- src/routes/cache.ts
+++ src/routes/cache.ts
@@ -1,30 +1,36 @@
+/* eslint-disable linebreak-style */
 import fs from 'fs';
-import filepath from './filepath';
 import sharp from 'sharp';
+// eslint-disable-next-line consistent-return
 
+import filepath from './filepath';
 
-const cache =async (width: number, height: number, imageName: string): Promise<string | undefined> => {
-    try {
-        // checking if image already exist 
-        const storedImages = `${filepath.storedImages}/${imageName}.jpeg`;
-        const newImages = `${filepath.newImages}/${imageName}_${width}_${height}.jpeg`
-        if (
-            fs.existsSync(newImages)
-        ) {
-            return newImages
-            
-        }
-        if (
-            await sharp(storedImages ).resize(width, height).jpeg().toFile(newImages)){
-           // console.log(newImages)
-      
-            return newImages
-        };
-        
-        
-    } catch (error) {
-        throw new Error(`can't find image ${error}`)
+const cache = async (
+  width: number,
+  height: number,
+  // eslint-disable-next-line consistent-return
+  imageName: string,
+)
+  // eslint-disable-next-line consistent-return
+  : Promise<string | undefined> => {
+  try {
+    // checking if image already exist
+    const storedImages = `${filepath.storedImages}/${imageName}.jpeg`;
+    const newImages = `${filepath.newImages}/${imageName}_${width}_${height}.jpeg`;
+    if (
+      fs.existsSync(newImages)
+    ) {
+      return newImages;
     }
-}
+    if (
+      await sharp(storedImages).resize(width, height).jpeg().toFile(newImages)) {
+      // console.log(newImages)
 
-export default cache
+      return newImages;
+    }
+  } catch (error) {
+    throw new Error(`can't find image ${error}`);
+  }
+};
+
+export default cache;
--- src/routes/strilizer.ts
+++ src/routes/strilizer.ts
@@ -1,42 +1,29 @@
-import  express  from "express";
+import express from 'express';
 
+// @eslint
 
+const sterilizer = async (req: express.Request, res: express.Response, next: express.NextFunction) => {
+  try {
+    const { width } = req.query;
+    const { height } = req.query;
+    const { imageName } = req.query;
 
+    if (
+      typeof width !== 'number'
+            || typeof height !== 'number') { typeof imageName !== 'string'; }
+    {
+      Number: numberConstructor(width);
+      Number(height);
+      // return res.send('correct parameter')
+    }
 
+    if (
+      width === undefined || height === undefined
+    ) { return res.send('incomplet parameter'); }
+    next();
+  } catch (error) {
+    return res.status(500).send(error);
+  }
+};
 
-//@eslint
-
-const sterilizer =  async  (req: express.Request, res: express.Response, next: express.NextFunction,) => {
-    try {
-        
-        let width = req.query.width;
-        let height = req.query.height;
-        let imageName = req.query.imageName;
-        
-        if (
-            typeof width != "number" ||
-            typeof height != "number")
-            typeof imageName != "string"
-    
-        {
-            Number(width);
-            Number(height)
-           //return res.send('correct parameter')
-        };
-
-        if (
-            width === undefined ||
-            height === undefined
-        )
-           return res.send('incomplet parameter')
-        else next();
-    } catch (error) {
-       return res.status(500).send(error)
-    
-    };
-}
-    
-
-
-
-    export default sterilizer ;
+export default sterilizer;
